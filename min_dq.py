134b7ae50d731e815874c82ae1d14143acda5b41
Waqar Saleem <waqar.saleem@sse.habib.edu.pk>
2018-01-19 21:46:36 +0500

    Added min_dq.py

---
+++ min_dq.py
@@ -0,0 +1,44 @@
+class MinDeque:
+    '''As described in Exercise 3.16.'''
+
+    def __init__():
+        pass
+
+    def __str__(self):
+        '''Returns a string representation of an object for printing.
+        '''
+        pass
+
+    def add_first(self, x):
+        '''Adds x at the front.
+        '''
+        pass
+
+    def add_last(self, x):
+        '''Adds x at the back.
+        '''
+        pass
+
+    def remove_first(self):
+        '''Removes the element at the front and returns it. Returns None
+        if dq is empty.
+        '''
+        pass
+
+    def remove_last(self):
+        '''Removes the element at the back and returns it. Returns None 
+        if dq is empty.
+        '''
+        pass
+
+    def size(self):
+        '''Returns the number of elements currently in the dq.
+        '''
+        pass
+
+    def min(self):
+        '''Returns the smallest element currently in the dq.
+        '''
+        pass
+
+    
