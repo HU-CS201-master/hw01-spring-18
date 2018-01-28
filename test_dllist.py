c43df507593e111b95daed143a836864f8c18c7c
Syed Shariq Ali <sa00509@st.habib.edu.pk>
2018-01-28 22:46:49 +0500

    fixed dllist test case.

--- test_dllist.py
+++ test_dllist.py
@@ -7,11 +7,11 @@ def test_add_get():
     assert dllist.add(1, 15) == True
     assert dllist.add(4, 40) == False
     assert dllist.add(-1, 40) == True
-    assert dllist.add(-3, 40) == False
+    assert dllist.add(-5, 40) == False
     assert dllist.get(0) == 10
     assert dllist.get(1) == 15
     assert dllist.get(2) == 20
-    assert dllist.get(3) == False
+    assert dllist.get(-1) == 40
 
 def test_add_front_back():
     dllist = DLList()
@@ -19,16 +19,13 @@ def test_add_front_back():
     assert dllist.add(1, 200) == True
     assert dllist.add(2, 150) == True
 
-    assert dllist.head.data == 100
-    assert dllist.tail.data == 150
+    assert dllist.dummy.front.data == 100
+    assert dllist.dummy.back.data == 150
     assert dllist.add(1, 333) == True
-    x=dllist.head.front
+    x=dllist.dummy.front.front
     assert x.back.data == 100
     assert x.front.data == 200
 
-    y=dllist.tail.front
-    assert y.back.data == 200
-    assert y.front == None
     
 def test_add_head_tail():
     dllist = DLList()
@@ -36,10 +33,10 @@ def test_add_head_tail():
     assert dllist.size == 1
     assert dllist.add(1, 200) == True
     assert dllist.add(2, 150) == True
-    assert dllist.head.data == 100
+    assert dllist.dummy.front.data == 100
     assert dllist.add(0, 250) == True
-    assert dllist.head.data == 250
-    assert dllist.tail.data == 150
+    assert dllist.dummy.front.data == 250
+    assert dllist.dummy.back.data == 150
     assert dllist.size == 4
     assert dllist.get(0) == 250
     assert dllist.get(1) == 100
@@ -59,7 +56,7 @@ def test_set():
     assert dllist.get(3)== 44
     assert dllist.set(-2, 25) == True
     assert dllist.get(-2) == 25
-    assert dllist.tail.data == 40
+    assert dllist.dummy.back.data == 40
     
 def test_remove():
     dllist= DLList()
@@ -70,12 +67,12 @@ def test_remove():
     dllist.add(3, 40)
     assert dllist.remove(1) ==20
     assert dllist.size == 3
-    assert dllist.head.data == 10
+    assert dllist.dummy.front.data == 10
     assert dllist.remove(0) ==10
-    assert dllist.head.data == 100
+    assert dllist.dummy.front.data == 100
     assert dllist.size == 2
     assert dllist.remove(1) ==40
-    assert dllist.tail.data == 100
+    assert dllist.dummy.back.data == 100
     assert dllist.size == 1
 
 def test_remove_front_back():
@@ -83,18 +80,15 @@ def test_remove_front_back():
     dllist.add(0, 100)
     dllist.remove(0) == 100
     dllist.remove(1) == None
-    dllist.add(1, 200)
-    dllist.add(2, 150)
-    dllist.add(3, 300)
+    dllist.add(0, 200)
+    dllist.add(1, 150)
+    dllist.add(2, 300)
     
-    assert dllist.remove(1) == 200
-    x = dllist.head.front
-    assert x.back.data == 100
-    assert x.front.data == 300
+    assert dllist.remove(0) == 200
+    x = dllist.dummy.front.front
+    assert x.back.data == 150
+    assert x.data == 300
 
-    y=dllist.tail.back
-    assert y.back.data == 100
-    assert y.front.data == 300
     
 def test_remove_head_tail():
     dllist = DLList()
@@ -104,63 +98,63 @@ def test_remove_head_tail():
     dllist.add(3, 40)
 
     assert dllist.remove(0) == 10
-    assert dllist.head.data == 20
-    assert dllist.tail.data == 40
+    assert dllist.dummy.front.data == 20
+    assert dllist.dummy.back.data == 40
     assert dllist.size == 3
     assert dllist.get(0) == 20
     assert dllist.get(1) == 100
 
 def test_is_palindrome():
     dllist = DLList()
-    a=["madam","discrete",")()("]
-    for i in a[0]:
-        dllist.add(i)
+    a=["madam","discrete","())("]
+    for i,x in enumerate(a[0]):
+        dllist.add(i,x)
     assert dllist.is_palindrome()== True
     dllist = DLList()
-    for i in a[1]:
-        dllist.add(i)
+    for i,x in enumerate(a[1]):
+        dllist.add(i,x)
     assert dllist.is_palindrome()== False
     dllist = DLList()
-    for i in a[2]:
-        dllist.add(i)
+    for i,x in enumerate(a[2]):
+        dllist.add(i,x)
     assert dllist.is_palindrome()== True
     
 def test_truncate():
     dllist = DLList()
     a=["abcdefghi","ct is awesome"]
-    for i in a[0]:
-        dllist.add(i)
+    for i,x in enumerate(a[0]):
+        dllist.add(i,x)
     dllist.truncate(5)
     assert dllist.get(0) == 'a'
-    assert dllist.get(-1) == 'f'
+    assert dllist.get(-1) == 'e'
     for i in range(5):
         dllist.get(i) == a[0][i]
     dllist = DLList()
-    for i in a[1]:
-        dllist.add(i)
+    for i,x in enumerate(a[1]):
+        dllist.add(i,x)
     dllist.truncate(7)
     assert dllist.get(0) == 'c'
-    assert dllist.get(-1) == 'w'
+    assert dllist.get(-1) == 'a'
     for i in range(7):
         dllist.get(i) == a[1][i]
-    assert dllist.tail.data == 'w'
+    assert dllist.dummy.back.data == 'a'
 
 def test_absorb():
     dllist = DLList()
     dllist2 = DLList()
     dllist3 = DLList()
     a=["Alice","Bob","Eve"]
-    for i in a[0]:
-        dllist.add(i)
-    for i in a[1]:
-        dllist2.add(i)
-    for i in a[2]:
-        dllist3.add(i)
+    for i,x in enumerate(a[0]):
+        dllist.add(i,x)
+    for i,x in enumerate(a[1]):
+        dllist2.add(i,x)
+    for i,x in enumerate(a[2]):
+        dllist3.add(i,x)
     dllist2.absorb(dllist3)
     x=a[1]+a[2]
     for i in range(len(x)):
         assert dllist2.get(i) == x[i]
-    assert dllist2.tail.data == 'e'
+    assert dllist2.dummy.back.data == 'e'
     y=a[0]+x
     dllist.absorb(dllist2)
     for i in range(len(y)):
@@ -171,16 +165,16 @@ def test_absorb():
 def test_reverse():
     dllist = DLList()
     a=["abcdefghi","programming is fun"]
-    for i in a[0]:
-        dllist.add(i)
+    for i,x in enumerate(a[0]):
+        dllist.add(i,x)
     dllist.reverse()
     assert dllist.get(1) == 'h'
     assert dllist.get(-2) == 'b'
     for i in range(len(a[0])):
         assert dllist.get(i) == a[0][-i-1]
     dllist = DLList()
-    for i in a[1]:
-        dllist.add(i)
+    for i,x in enumerate(a[1]):
+        dllist.add(i,x)
     dllist.reverse()
     assert dllist.get(0) == 'n'
     assert dllist.get(-1) == 'p'
